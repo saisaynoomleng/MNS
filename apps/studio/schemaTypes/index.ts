517f3a93d96afb0dde1a36df2b87ca19fa80a310
saisaynoomleng <saileng9723@gmail.com>
2026-09-21 21:41:13 -0400

    sanity_site_settings

--- apps/studio/schemaTypes/index.ts
+++ apps/studio/schemaTypes/index.ts
@@ -1,1 +1,28 @@
-export const schemaTypes = []
+import {
+  blockContent,
+  faq,
+  footerColumn,
+  imageWithAlt,
+  navDropdown,
+  navLink,
+  seo,
+  socialLink,
+  videoEmbedded,
+} from './shareTypes';
+import { siteSettings } from './singletons/site-settings';
+
+export const schemaTypes = [
+  // share Types
+  imageWithAlt,
+  blockContent,
+  seo,
+  socialLink,
+  faq,
+  navLink,
+  navDropdown,
+  footerColumn,
+  videoEmbedded,
+
+  //   singletons
+  siteSettings,
+];
--- apps/studio/structure.ts
+++ apps/studio/structure.ts
@@ -1,4 +1,13 @@
 import { StructureResolver } from 'sanity/structure';
 
+import { IoSettingsOutline } from 'react-icons/io5';
+
 export const structure: StructureResolver = (S) =>
-  S.list().title('MNS').items([]);
+  S.list()
+    .title('MNS')
+    .items([
+      S.divider().title('Operations'),
+      S.documentTypeListItem('siteSettings')
+        .title('Site Settings')
+        .icon(IoSettingsOutline),
+    ]);
